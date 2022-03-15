88a9a67219502c0c6caf506477ac7d32adf23714
Celeaxy <erich.sartison@web.de>
2022-03-15 14:38:09 +0000

    fixed resizing bug, code leaving input

--- TryJuri/src/routes/TryOut.tsx
+++ TryJuri/src/routes/TryOut.tsx
@@ -1,13 +1,13 @@
-import { ChangeEvent, KeyboardEvent, SyntheticEvent, useState, ReactNode, useEffect, useCallback} from 'react';
+import { ChangeEvent, KeyboardEvent, useState, ReactNode, useEffect, useCallback, useRef, RefObject } from 'react';
 import '../style/App.scss';
-import { Button, TextField , CircularProgress} from '@mui/material';
+import { Button, TextField, CircularProgress } from '@mui/material';
 import { Theme } from '@mui/material';
 import axios from 'axios';
 import Highlighter from '../util/Highlighter';
 import { CSSProperties } from 'react';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import base64url from 'base64url';
-
+import { useResizeDetector } from 'react-resize-detector';
 (window as any).global = window;
 // @ts-ignore
 window.Buffer = window.Buffer || require('buffer').Buffer;
@@ -21,17 +21,17 @@ export default function TryOut({ theme }: { theme?: Theme }) {
   let [loading, setLoading] = useState(false);
 
   let handleRun = function () {
-    if(!code){
-      setOutput(''); 
+    if (!code) {
+      setOutput('');
       return;
     }
     let encoded = base64url(code);
-    
+
     setLoading(true);
     axios.get('https://juri-online-compiler.herokuapp.com/jurii?code=' + encoded)
-    .then(res =>  setOutput(output + res.data.standard + res.data.error))
-    .catch(err => setOutput(output + err))
-    .finally(() => setLoading(false));
+      .then(res => setOutput(output + res.data.standard + res.data.error))
+      .catch(err => setOutput(output + err))
+      .finally(() => setLoading(false));
 
     /*axios.get('https://icanhazdadjoke.com/search?term=' + code, { headers: { 'Accept': 'application/json' } })
       .then(res => setOutput(res.data.results.map((r: DadJokesResult) => r.joke).join('\n\n')))
@@ -45,8 +45,8 @@ export default function TryOut({ theme }: { theme?: Theme }) {
         <Editor callback={setCode} />
         <TextField label='Output' multiline margin='normal' variant='outlined' style={{ width: '40%', minWidth: '400px', margin: '2%' }} rows='25' value={output} disabled />
       </div>
-      <Button variant='contained' onClick={handleRun} style={{fontSize:'20px'}}>
-        {loading ? <CircularProgress size='1.7em'/> : <><PlayArrowIcon fontSize='large'/>Run</>}</Button>
+      <Button variant='contained' onClick={handleRun} style={{ fontSize: '20px' }}>
+        {loading ? <CircularProgress size='1.7em' /> : <><PlayArrowIcon fontSize='large' />Run</>}</Button>
     </div>
 
   );
@@ -58,8 +58,8 @@ declare interface editorProps {
 }
 
 function Editor({ callback, autoFocus }: editorProps) {
-  
-  let [highlighted, setHighlighted] = useState(localStorage.getItem('code') && <>{highlight(localStorage.getItem('code')!)}</>|| <></>);
+
+  let [highlighted, setHighlighted] = useState(localStorage.getItem('code') && <>{highlight(localStorage.getItem('code')!)}</> || <></>);
   let [text, setText] = useState(localStorage.getItem('code') || '');
   let [scrolling, setScrolling] = useState(0);
   let handleChange = function (event: ChangeEvent<HTMLTextAreaElement>) {
@@ -70,7 +70,7 @@ function Editor({ callback, autoFocus }: editorProps) {
       setHighlighted(<>{highlight(event.target.value)}</>);
     }
   }
-
+  let editor = useRef<HTMLDivElement>(null);
   let handleKeyPress = function (event: KeyboardEvent<HTMLDivElement>) {
     let t = event.target as HTMLTextAreaElement;
     let tab = '    ';
@@ -80,13 +80,13 @@ function Editor({ callback, autoFocus }: editorProps) {
     }
     let currentRow = () => {
       let rows = t.value.slice(0, cpos).split('\n');
-      return rows[rows.length-1];
+      return rows[rows.length - 1];
     }
     switch (event.key) {
       case 'Tab':
         event.preventDefault();
         insert(tab);
-        t.setSelectionRange(cpos+4, cpos+4);
+        t.setSelectionRange(cpos + 4, cpos + 4);
         break;
       case 'Enter':
         event.preventDefault();
@@ -95,41 +95,41 @@ function Editor({ callback, autoFocus }: editorProps) {
         if (matches) {
           spaces = matches[0].length;
         }
-        if (currentRow().match(/^\s*(if|fun|operator|:\w+\s*=\s*init\s+\.+\s+as\s+\w+\.*|iterate\s+.+\sas\s+.+)/)){
-          spaces+=4;
+        if (currentRow().match(/^\s*(if|fun|operator|:\w+\s*=\s*init\s+\.+\s+as\s+\w+\.*|iterate\s+.+\sas\s+.+)/)) {
+          spaces += 4;
         }
         insert('\n' + ' '.repeat(spaces));
-        t.setSelectionRange(cpos+spaces+1, cpos+spaces+1);
+        t.setSelectionRange(cpos + spaces + 1, cpos + spaces + 1);
         break;
       case 'Backspace':
-        if(currentRow().match(/ {4}$/)){
+        if (currentRow().match(/ {4}$/)) {
           event.preventDefault();
-          t.value = t.value.slice(0,cpos-4) + t.value.slice(cpos);
-          t.setSelectionRange(cpos-4, cpos-4);
+          t.value = t.value.slice(0, cpos - 4) + t.value.slice(cpos);
+          t.setSelectionRange(cpos - 4, cpos - 4);
         }
         break;
     }
     //t.dispatchEvent(new Event('change'));
-    handleChange({target: {name: t.name, value : t.value}} as ChangeEvent<HTMLTextAreaElement>);
+    handleChange({ target: { name: t.name, value: t.value } } as ChangeEvent<HTMLTextAreaElement>);
   }
   const handleScroll = useCallback(event => {
     setScrolling(event.target.scrollTop || 0);
   }, []);
 
-useEffect(() => {
-  let editor = document.getElementById('editor')!;
+  useEffect(() => {
+    let editor = document.getElementById('editor')!;
     editor.addEventListener("scroll", handleScroll);
     return () => {
-        editor.removeEventListener("scroll", handleScroll);
+      editor.removeEventListener("scroll", handleScroll);
     };
-}, [handleScroll]);
+  }, [handleScroll]);
 
-  useEffect(() =>{
+  useEffect(() => {
 
-  },[(document.getElementById('editor') as HTMLTextAreaElement)?.value]);
+  }, [(document.getElementById('editor') as HTMLTextAreaElement)?.value]);
   return <>
-    <TextField sx={{ color: 'transparent !important' }} id='editor' placeholder='Please enter your code here.' onScroll={handleScroll} onKeyDown={handleKeyPress} inputProps={{ spellCheck: 'false' }} autoFocus={autoFocus} multiline onChange={handleChange} margin='normal' variant='outlined' style={{ width: '40%', minWidth: '400px', margin: '2%' }} rows='25' value={text} />
-    <DivOverlay elementID={'editor'} scrolling={scrolling} >{highlighted}</DivOverlay>
+    <TextField sx={{ color: 'transparent !important' }} ref={editor} id='editor' placeholder='Please enter your code here.' onScroll={handleScroll} onKeyDown={handleKeyPress} inputProps={{ spellCheck: 'false' }} autoFocus={autoFocus} multiline onChange={handleChange} margin='normal' variant='outlined' style={{ width: '40%', minWidth: '400px', margin: '2%' }} rows='25' value={text} />
+    <DivOverlay elementID={'editor'} scrolling={scrolling} targetRef={editor}>{highlighted}</DivOverlay>
   </>
 }
 
@@ -140,7 +140,7 @@ function highlight(text: string) {
   const numbers = { regex: /\d+/, color: 'rgb(230,255,200)' };
   const lists = { regex: /:[A-Za-z]\w*/, color: 'rgb(255,200,120)' };
   const operators = { regex: /[+\-*/><.=!%|&]/, color: 'rgb(100,255,255)' };
-  const comments = {regex: /#.*/, color: 'rgb(150,150,150)'}
+  const comments = { regex: /#.*/, color: 'rgb(150,150,150)' }
   const hl = new Highlighter(
     { regex: /if/, color: 'rgb(255,100,80)' },
     keywords,
@@ -154,7 +154,7 @@ function highlight(text: string) {
   return hl.highlight(text);
 }
 
-function DivOverlay({ elementID, children, scrolling}: { elementID: string, children: ReactNode, scrolling : number}) {
+function DivOverlay({ elementID, children, scrolling, targetRef }: { elementID: string, children: ReactNode, scrolling: number, targetRef: RefObject<HTMLDivElement> }) {
   let [style, setStyle] = useState({
     width: '0',
     height: '0',
@@ -170,8 +170,13 @@ function DivOverlay({ elementID, children, scrolling}: { elementID: string, chil
     left: 0,
     overflowY: 'clip',
     whiteSpace: 'pre-wrap'
-    
+
   } as CSSProperties);
+
+  const onResize = useCallback(() => {
+    updateStyle();
+  }, []);
+  const { width, height } = useResizeDetector({ targetRef, onResize });
 
   let updateStyle = function () {
     let element = document.getElementById(elementID)!;
@@ -187,6 +192,7 @@ function DivOverlay({ elementID, children, scrolling}: { elementID: string, chil
       left: rect.left + window.scrollX
     } as CSSProperties);
   }
+
   window.onscroll = updateStyle;
   window.onresize = updateStyle;
   window.onload = updateStyle;
@@ -195,5 +201,5 @@ function DivOverlay({ elementID, children, scrolling}: { elementID: string, chil
     updateStyle();
   }, [(document.getElementById(elementID) && (getComputedStyle(document.getElementById(elementID)!).top, getComputedStyle(document.getElementById(elementID)!).left))]);
 
-  return <div style={style}><div className='highlighting' style={{transform : `translateY(${-scrolling}px)`}}>{children}</div></div>
+  return <div style={style}><div className='highlighting' style={{ transform: `translateY(${-scrolling}px)` }}>{children}</div></div>
 }
